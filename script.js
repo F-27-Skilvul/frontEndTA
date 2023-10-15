29d034c3efe6887fc19fa3fa1f1f08b55d2de959
Andiagung <11211011@student.itk.ac.id>
2023-10-15 21:21:52 +0800

    final landing page

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-// Navbar fixed
-window.onscroll = function () {
-    const header = document.querySelector("header");
-    const fixedNav = header.offsetTop;
-  
-    if ((window, pageYOffset > fixedNav)) {
-      header.classList.add("navbar-fixed");
-    } else {
-      header.classList.remove("navbar-fixed");
-    }
-  };
-  
-  // Hamburger
-  const hamburger = document.querySelector("#hamburger");
-  const navMenu = document.querySelector("#nav-menu");
-  
-  hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("hidden");
+
+// Simpan elemen navbar dalam variabel
+// Simpan elemen navbar dalam variabel
+const navbar = document.querySelector('.bg-white');
+
+// Simpan tinggi navbar
+const navbarHeight = navbar.offsetHeight;
+
+// Fungsi untuk menentukan apakah navbar harus ditampilkan atau disembunyikan saat menggulir
+function toggleNavbarOnScroll() {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('fixed', 'top-0', 'left-0', 'right-0', 'z-50', 'shadow-md', 'blur-navbar');
+  } else {
+    navbar.classList.remove('fixed', 'top-0', 'left-0', 'right-0', 'z-50', 'shadow-md', 'blur-navbar');
+  }
+}
+
+// Tambahkan event listener ke window untuk memanggil fungsi saat menggulir
+window.addEventListener('scroll', toggleNavbarOnScroll);
+
+// Panggil fungsi saat halaman pertama kali dimuat
+toggleNavbarOnScroll();
+
+
+
+//Navbar navigation
+document.addEventListener("DOMContentLoaded", function () {
+  // Button user menu dan dropdown
+  const userMenuButton = document.getElementById("user-menu-button");
+  const userDropdown = document.getElementById("user-dropdown");
+
+  userMenuButton.addEventListener("click", function () {
+    userDropdown.classList.toggle("hidden");
   });
+
+  // Mobile 
+  const mobileMenuButton = document.querySelector("[data-collapse-toggle='navbar-user']");
+  const navbarUser = document.getElementById("navbar-user");
+
+  mobileMenuButton.addEventListener("click", function () {
+    navbarUser.classList.toggle("hidden");
+  });
+});
+
+// You can also add additional functionality here as needed.
